e4927b6a28ee973bbb3f03492e13aeddaabb7405
Sanchita <guptasanchita28@gmail.com>
2026-02-12 21:20:24 +0530

    Update 2q4.cpp

--- Sem4/Programming_Paradigms_Lab/2assign/2q4.cpp
+++ Sem4/Programming_Paradigms_Lab/2assign/2q4.cpp
@@ -1,32 +1,84 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 template <typename T>
-void genericSort(T arr[], int n) {
+//genericSort
+void bubbleSort(T arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                T temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
         }
-        T temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
+        if (!swapped) break;
     }
 }
 
-struct Student {
-    int roll;
-};
+template <typename T>
+void printArray(T arr[], int n) {
+    cout << "Sorted Array: [ ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i];
+        if (i < n - 1) cout << ", ";
+    }
+    cout << " ]" << endl;
+}
+
+template <typename T>
+void takeInput(T arr[], int n) {
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+}
+
 
 int main() {
-    int intArr[] = {5, 2, 9, 1};
-    genericSort(intArr, 4);
+    int nInt;
+    cout << "Integer Sort" << endl;
+    cout << "Enter the number of integers: ";
+    cin >> nInt;
 
-    double doubleArr[] = {3.5, 1.2, 7.8};
-    genericSort(doubleArr, 3);
+    int* intArr = new int[nInt];
 
-    Student stArr[2];
-    genericSort(stArr, 2); // Compilation Error: operator< not defined
+    takeInput(intArr, nInt);
+    bubbleSort(intArr, nInt);
+    printArray(intArr, nInt);
+
+    delete[] intArr;
+    cout << endl;
+
+    int nDouble;
+    cout << "Double Sort" << endl;
+    cout << "Enter the number of doubles: ";
+    cin >> nDouble;
+
+    double* doubleArr = new double[nDouble];
+
+    takeInput(doubleArr, nDouble);
+    bubbleSort(doubleArr, nDouble);
+    printArray(doubleArr, nDouble);
+
+    delete[] doubleArr;
+    cout << endl;
+
+    int nString;
+    cout << "String Sort" << endl;
+    cout << "Enter the number of strings: ";
+    cin >> nString;
+
+    string* stringArr = new string[nString];
+
+    takeInput(stringArr, nString);
+    bubbleSort(stringArr, nString);
+    printArray(stringArr, nString);
+
+    delete[] stringArr;
+
     return 0;
 }
